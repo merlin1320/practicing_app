be1e4d055eebbf45e9f67177b7d1efae70299444
merlin1320 <littlenights447@gmail.com>
2023-12-07 20:42:07 -0700

    Updated webpage with bs

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,11 @@ function App() {
           Relearning to code
         </h1>
           <p>
-            I am learning how to code again after a 10 year haitus. I am <Birthday birthdate="1992-01-09" /> years old. 
+            I am learning how to code again after a 10 year haitus. I am <Birthday birthdate="1992-01-09"/> years old. 
+            {/* Vinny is <Birthday birthdate='1991-01-23'/> years old */}
           </p>
           <p >
             <img src={Loki} className='loki-photo' alt='Loki'/>
-            
           </p>
           <p className='text-talk'>
             This is my old cat Loki. He was a great cat even though he liked to play around. 
@@ -25,7 +25,7 @@ function App() {
             I have no clue what I am doing
           </p>
           <p>
-             <a href="https://github.com/merlin1320/practicing_app">Here is the code I am working on</a>
+             <a href="https://github.com/merlin1320/practicing_app" target='_blank'>Here is the code I am working on</a>
           </p>
       </div>
     </>
