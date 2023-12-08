2b3a1017d3965d3786bc2d0ff20c86957dc932e9
merlin1320 <littlenights447@gmail.com>
2023-12-07 20:08:54 -0700

    Updating minor changes
    Changed the birthday function

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,32 @@
-// import { useEffect, useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 import Loki from './assets/Loki.jpg'
 import {Birthday} from './Birthday'
-// import { Button } from './Button'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
 
   return (
     <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-
       <div>
         <h1>
           Relearning to code
         </h1>
           <p>
-            I am learning how to code again after a 10 year haitus. I am <Birthday/> years old. 
-            {/* <Button children={undefined}/> */}
+            I am learning how to code again after a 10 year haitus. I am <Birthday birthdate="1992-01-09" /> years old. 
           </p>
           <p >
             <img src={Loki} className='loki-photo' alt='Loki'/>
+            
+          </p>
+          <p className='text-talk'>
             This is my old cat Loki. He was a great cat even though he liked to play around. 
           </p>
           <p>
             I have no clue what I am doing
           </p>
-          <a href="https://github.com/merlin1320/practicing_app">Here is the code I am working on</a>
+          <p>
+             <a href="https://github.com/merlin1320/practicing_app">Here is the code I am working on</a>
+          </p>
       </div>
     </>
   )
--- src/Birthday.tsx
+++ src/Birthday.tsx
@@ -1,10 +1,13 @@
+// import { ReactNode } from "react";
+
+// const myBday = new Date('1992-01-09');
+// const age = Math.floor((today.getTime() - myBday.getTime())/ (1000 * 60 * 60 * 24 * 365))
+
+
+
 const today = new Date();
-const myBday = new Date('1992-01-09');
-const age = Math.floor((today.getTime() - myBday.getTime())/ (1000 * 60 * 60 * 24 * 365))
-
-
-
-
-export function Birthday() {
+export function Birthday(props: {birthdate: string}) {
+    const newDate = new Date(props.birthdate);
+    const age = Math.floor((today.getTime() - newDate.getTime())/ (1000 * 60 * 60 * 24 * 365))
     return age
 }
